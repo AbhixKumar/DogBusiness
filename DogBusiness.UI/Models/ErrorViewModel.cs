5274ec30adf9c38ae25c16e8961c09c8f481c437
Abhi Kumar <abhishekumarfvtc@gmail.com>
2021-03-14 14:49:59 -0500

    trying

---
+++ DogBusiness.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DogBusiness.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
